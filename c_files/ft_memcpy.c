e8325292113a0ed4ae4c6da7d9d9177551fd69e1
prometheus <shaunng_ys@proton.me>
2024-11-18 16:26:40 +0800

    Added ft_bzero & ft_memcpy

---
+++ c_files/ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shaun <sng@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 16:20:34 by shaun             #+#    #+#             */
+/*   Updated: 2024/11/18 16:20:42 by shaun             ###   ########kl       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void    *ft_memcpy(void *dest_str, const void *src_str, size_t n);
+
+int main(void)
+{
+
+}
+
+void    *ft_memcpy(void *dest_str, const void *src_str, size_t n)
+{
+
+}
