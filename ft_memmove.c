8f029d5fffe6d3517a3cb4b46a3c86962017f308
Shaun Ng <shaunng_ys@proton.me>
2025-01-21 18:08:10 +0800

    Norm checked ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -22,30 +22,31 @@ int main(void)
     printf("This is the source string: %s\n", source_string);
     printf("This is the destination string: %s\n", destination_string);
     ft_memmove(destination_string, source_string, nbr);
-    printf("This is the destination string after being passed through the ft_memmove function: %s\n", destination_string);
+    printf("This is the destination string: %s\n", destination_string);
     return (0);
 }
 */
-void    *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    unsigned int    i;
-    unsigned int    nbr;
-    void *temp = (char *) malloc(n*sizeof(char));
+	unsigned int	i;
+	unsigned int	nbr;
+	void			*temp;
 
-    i = 0;
-    nbr = n;
-    while (nbr)
-    {
-        ((char *)temp)[i] = ((char *)src)[i];
-        i++;
-        nbr--;
-    }
-    i = 0;
-    while (n)
-    {
-        ((char *)dest)[i] = ((char *)temp)[i];
-        i++;
-        n--;
-    }
-    return (dest);
+	i = 0;
+	nbr = n;
+	temp = (char *) malloc(n * sizeof(char));
+	while (nbr)
+	{
+		((char *)temp)[i] = ((char *)src)[i];
+		i++;
+		nbr--;
+	}
+	i = 0;
+	while (n)
+	{
+		((char *)dest)[i] = ((char *)temp)[i];
+		i++;
+		n--;
+	}
+	return (dest);
 }
