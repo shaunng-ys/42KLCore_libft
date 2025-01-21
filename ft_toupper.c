e7a197381c49b5c404b247bd7cc7e085e0e873e5
Shaun Ng <shaunng_ys@proton.me>
2025-01-21 21:04:43 +0800

    Norm checked ft_toupper.c

--- ft_toupper.c
+++ ft_toupper.c
@@ -20,11 +20,11 @@ int main(void)
     return (0);
 }
 */
-int ft_toupper(int c)
+int	ft_toupper(int c)
 {
-    if (c > 96 && c < 123)
-    {
-        c = c - 32;
-    }
-    return (c);
+	if (c > 96 && c < 123)
+	{
+		c = c - 32;
+	}
+	return (c);
 }
